bbbc03132628faf2213d75c354ee4b721215322c
srub <shani.rubovitch@gmail.com>
2020-04-14 14:12:21 +0200

    working, two tests

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,21 @@ package com.company;
 
 public class Main {
 
+    public static String errorcode(String code) {
+        long numOfErrors = code.chars().filter(c -> c >= 97 && c <=109).count();
+
+        return ( String.valueOf(numOfErrors) + "/" + code.length());
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        String s = "amamamamam";
+
+        System.out.println(s + " returns: " + errorcode(s));
+
+        s = "mmxx";
+        System.out.println(s + " returns: " + errorcode(s));
+
+
     }
 }
